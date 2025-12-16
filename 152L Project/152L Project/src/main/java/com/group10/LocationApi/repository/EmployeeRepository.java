bd20cccb0971ba424acd6bbcbca8c8e5a65731f8
Cordero, Ricci Ayn D. <142783519+aynaynayn@users.noreply.github.com>
2025-12-16 14:31:24 +0800

    Delete 152L Project directory

--- 152L Project/152L Project/src/main/java/com/group10/LocationApi/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group10.LocationApi.repository;
-
-import com.group10.LocationApi.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
